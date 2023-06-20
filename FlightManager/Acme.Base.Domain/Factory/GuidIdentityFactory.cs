9eecce941c9de2bbe95d691eb6caf047f777b471
Čavlek, Ivan <i.cavlek@douglas.de>
2023-06-20 14:08:05 +0200

    Switched to MassTransit.NewId for generating GUIDs

---
+++ FlightManager/Acme.Base.Domain/Factory/GuidIdentityFactory.cs
@@ -0,0 +1,10 @@
+﻿using MassTransit;
+using System;
+
+namespace Acme.Base.Domain.Factory;
+
+public sealed class GuidIdentityFactory : IIdentityFactory<Guid>
+{
+    Guid IIdentityFactory<Guid>.CreateIdentity() =>
+        NewId.NextSequentialGuid();
+}

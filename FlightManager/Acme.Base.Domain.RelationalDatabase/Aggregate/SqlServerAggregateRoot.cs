a32829fa15994a697a9eb7eef4585c2e87e6495b
Čavlek, Ivan <i.cavlek@douglas.de>
2023-06-20 10:51:28 +0200

    Base for relational databases created

---
+++ FlightManager/Acme.Base.Domain.RelationalDatabase/Aggregate/SqlServerAggregateRoot.cs
@@ -0,0 +1,13 @@
+﻿using Acme.Base.Domain.Aggregate;
+using Acme.Base.Domain.ValueObject;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Acme.Base.Domain.RelationalDatabase.Aggregate;
+
+public class SqlServerAggregateRoot<TKey> : AggregateRoot<TKey>
+    where TKey : IdValueObject
+{
+    [ConcurrencyCheck]
+    protected Guid Version { get; private set; }
+}
